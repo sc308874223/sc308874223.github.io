f37f9c9db38707ac334674e5b5c4895a99842eea
sc308874223 <308874223@qq.com>
2019-02-25 15:13:34 +0800

    init files

---
+++ files/企业档案查询（集团）/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j],k,_(l,m,n,o,p,q,r,_(),s,_(t,u,v,w,x,_(y,z,A,B),C,null,D,w,E,w,F,G,H,null,I,J,K,L,M,N,O,J),P,_(),Q,_(R,_(S,T,U,[_(S,V,W,g,X,[_(Y,Z,S,ba,bb,_(bc,k,b,bd,be,bf),bg,bh)])])),bi,_(bj,[])),bk,_(),bl,_());}; 
+var b="url",c="企业档案查询（集团）.html",d="generationDate",e=new Date(1551077708119.56),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="page",l="packageId",m="5e1ba5ec5d2c4fe9befe4b697d3bdf13",n="type",o="Axure:Page",p="name",q="企业档案查询（集团）",r="notes",s="style",t="baseStyle",u="627587b6038d43cca051c114ac41ad32",v="pageAlignment",w="near",x="fill",y="fillType",z="solid",A="color",B=0xFFFFFFFF,C="image",D="imageHorizontalAlignment",E="imageVerticalAlignment",F="imageRepeat",G="auto",H="favicon",I="sketchFactor",J="0",K="colorStyle",L="appliedColor",M="fontName",N="Applied Font",O="borderWidth",P="adaptiveStyles",Q="interactionMap",R="onLoad",S="description",T="页面载入 时",U="cases",V="Case 1",W="isNewIfGroup",X="actions",Y="action",Z="linkWindow",ba="在 当前窗口 打开 子公司档案查询",bb="target",bc="targetType",bd="子公司档案查询.html",be="includeVariables",bf=true,bg="linkType",bh="current",bi="diagram",bj="objects",bk="masters",bl="objectPaths";
+return _creator();
+})());
